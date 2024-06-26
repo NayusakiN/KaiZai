f8c30e71bc43dd5d5cddbf13dc2b1ca174240d3b
Nazar Maslianka <maslianka@lqlabs.net>
2024-06-26 23:02:09 +0300

    Updated all services to .Net8 and some fixes

--- src/Services.Common/KaiZai.Service.Common.MessageExchangeBaseConfigurator/MassTransit/Extensions.cs
+++ src/Services.Common/KaiZai.Service.Common.MessageExchangeBaseConfigurator/MassTransit/Extensions.cs
@@ -1,8 +1,6 @@
 using System.Reflection;
-using GreenPipes;
 using KaiZai.Service.Common.MessageExchangeBaseConfigurator.Settings;
 using MassTransit;
-using MassTransit.Definition;
 using MassTransit.RabbitMqTransport;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,7 +14,7 @@ public static class Extensions
     /// <remarks> 
     /// Important! Do not forget to configure ServiceSettings and RabbitMQSettings sections in your project.
     /// </remarks>
-    /// <param name="collection">The <see cref="IServiceCollection"/> to which MassTransit configuration will be added.</param>
+    /// <param name="services">The <see cref="IServiceCollection"/> to which MassTransit configuration will be added.</param>
     /// <param name="serviceSettings">The settings related to the microservice.</param>
     /// <param name="rabbitMQSettings">The settings related to the RabbitMQ message broker.</param>
     /// <param name="rabbitMqAdditionalConfigurations">An optional action for additional MassTransit and RabbitMQ configurations.</param>
@@ -26,7 +24,7 @@ public static class Extensions
     /// </param>
     /// <returns>The modified <see cref="IServiceCollection"/> with MassTransit configuration.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="serviceSettings"/> or <paramref name="rabbitMQSettings"/> is null.</exception>
-    public static IServiceCollection AddMassTransitCoreSetUp(this IServiceCollection collection,
+    public static IServiceCollection AddMassTransitCoreSetUp(this IServiceCollection services,
         ServiceSettings serviceSettings, 
         RabbitMQSettings rabbitMQSettings,
         Action<IBusRegistrationContext, IRabbitMqBusFactoryConfigurator>? rabbitMqAdditionalConfigurations = null,
@@ -37,7 +35,7 @@ public static class Extensions
             throw new ArgumentNullException($"{nameof(serviceSettings)} is missing in the configuration.");
         }
 
-        collection.AddMassTransit(configure => 
+        services.AddMassTransit(configure => 
         {
             assembliesConsumers = assembliesConsumers.Any() == true ? assembliesConsumers : new Assembly[] { Assembly.GetEntryAssembly() };
             configure.AddConsumers(assemblies: assembliesConsumers);
@@ -60,8 +58,13 @@ public static class Extensions
             });
         });
 
-        collection.AddMassTransitHostedService();
+        services.Configure<MassTransitHostOptions>(options =>
+        {
+            options.WaitUntilStarted = true;
+            options.StartTimeout = TimeSpan.FromSeconds(30);
+            options.StopTimeout = TimeSpan.FromMinutes(1);
+        });
 
-        return collection;
+        return services;
     }
 }
