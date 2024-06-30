bf38b42302ffce6b84a8dbea1c5595ecdac44f86
Nazar Maslianka <maslianka@lqlabs.net>
2024-06-30 12:45:02 +0300

    KaiZai.Incomes | DAL | Minor refactoring in models

--- src/Services/KaiZai.Service.Incomes/KaiZai.Service.Incomes.DAL/Models/Category.cs
+++ src/Services/KaiZai.Service.Incomes/KaiZai.Service.Incomes.DAL/Models/Category.cs
@@ -7,6 +7,6 @@ public sealed class Category : IEntity
 {
     public Guid Id { get; set; }
     public Guid ProfileId { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public CategoryType CategoryType { get; set; }
 }
--- src/Services/KaiZai.Service.Incomes/KaiZai.Service.Incomes.DAL/Models/Income.cs
+++ src/Services/KaiZai.Service.Incomes/KaiZai.Service.Incomes.DAL/Models/Income.cs
@@ -2,12 +2,12 @@ using KaiZai.Service.Common.MongoDataAccessRepository.Core;
 
 namespace KaiZai.Service.Incomes.DAL.Models;
 
-public class Income : IEntity
+public sealed class Income : IEntity
 {
     public Guid Id { get; set; }
     public Guid ProfileId { get; set; } 
     public Guid CategoryId { get; set; } 
     public DateTimeOffset IncomeDate { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public decimal Amount { get; set; }
 }
