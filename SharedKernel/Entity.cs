d06629cec2c4e8a507e09fdfbb3d2c85b262069a
Nazar Maslianka <maslianka@lqlabs.net>
2025-04-21 17:07:39 +0300

    add app's shared kernel objects

---
+++ SharedKernel/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedKernel;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public List<IDomainEvent> DomainEvents => [.. _domainEvents];
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    public void Raise(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
---
+++ SharedKernel/IDomainEvent.cs
@@ -0,0 +1,5 @@
+using MediatR;
+
+namespace SharedKernel;
+
+public interface IDomainEvent : INotification;
