ccdd781202ba913782afa4052e5d4b792d1e0bf4
Nazar Maslianka <maslianka@lqlabs.net>
2025-04-29 15:36:45 +0300

    refact Entity object

--- SharedKernel/Entity.cs
+++ SharedKernel/Entity.cs
@@ -2,17 +2,6 @@
 
 public abstract class Entity
 {
-    private readonly List<IDomainEvent> _domainEvents = [];
+    public Guid Id { get; set; }
 
-    public List<IDomainEvent> DomainEvents => [.. _domainEvents];
-
-    public void ClearDomainEvents()
-    {
-        _domainEvents.Clear();
-    }
-
-    public void Raise(IDomainEvent domainEvent)
-    {
-        _domainEvents.Add(domainEvent);
-    }
 }
