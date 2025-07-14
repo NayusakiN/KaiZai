d28f90186a9fa7905d24b6550487ed1eac38d611
Nazar Maslianka <maslianka@lqlabs.net>
2025-07-14 16:06:00 +0300

    rm IPipelineBehavior implementation with just <in TInput>

--- Application/Contracts/Mediator/IPipelineBehavior.cs
+++ Application/Contracts/Mediator/IPipelineBehavior.cs
@@ -6,8 +6,3 @@ public interface IPipelineBehavior<in TInput, TOutput>
 {
     Task<Result<TOutput>> HandleAsync(TInput input, Func<Task<Result<TOutput>>> next, CancellationToken cancellationToken = default);
 }
-
-public interface IPipelineBehavior<in TInput>
-{
-    Task<Result> HandleAsync(TInput input, Func<Task<Result>> next, CancellationToken cancellationToken = default);
-}
