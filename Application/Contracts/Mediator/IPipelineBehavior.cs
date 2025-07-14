b3d2097f948b0e3c4658d18add9135a198b7c5c1
Nazar Maslianka <maslianka@lqlabs.net>
2025-07-14 16:06:46 +0300

    add IRequest<TOutput> as input T parameter for IPipelineBehavior

--- Application/Contracts/Mediator/IPipelineBehavior.cs
+++ Application/Contracts/Mediator/IPipelineBehavior.cs
@@ -3,6 +3,7 @@ using SharedKernel;
 namespace Application.Contracts.Mediator;
 
 public interface IPipelineBehavior<in TInput, TOutput>
+    where TInput : IRequest<TOutput>
 {
     Task<Result<TOutput>> HandleAsync(TInput input, Func<Task<Result<TOutput>>> next, CancellationToken cancellationToken = default);
 }
