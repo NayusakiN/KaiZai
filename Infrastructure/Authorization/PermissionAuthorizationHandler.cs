4d7f99e7e1baf512c31f62c1e97cd1589d470852
Nazar Maslianka <maslianka@lqlabs.net>
2025-07-14 17:38:29 +0300

    add Authorization related important methods

---
+++ Infrastructure/Authorization/PermissionAuthorizationHandler.cs
@@ -0,0 +1,38 @@
+using Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Authorization;
+
+internal sealed class PermissionAuthorizationHandler(IServiceScopeFactory serviceScopeFactory)
+    : AuthorizationHandler<PermissionRequirement>
+{
+    protected override async Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        PermissionRequirement requirement)
+    {
+        // TODO: You definitely want to reject unauthenticated users here.
+        if (context.User is not { Identity.IsAuthenticated: true } or { Identity.IsAuthenticated: false })
+        {
+            // TODO: Remove this call when you implement the PermissionProvider.GetForUserIdAsync
+            context.Succeed(requirement);
+
+            return;
+        }
+
+        using IServiceScope scope = serviceScopeFactory.CreateScope();
+
+        PermissionProvider permissionProvider = scope.ServiceProvider.GetRequiredService<PermissionProvider>();
+
+        Guid userId = context.User.GetUserId();
+
+        HashSet<string> permissions = await permissionProvider.GetForUserIdAsync(userId);
+
+        if (permissions.Contains(requirement.Permission))
+        {
+            context.Succeed(requirement);
+
+            return;
+        }
+    }
+}
---
+++ Infrastructure/Authorization/PermissionAuthorizationPolicyProvider.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Authorization;
+
+internal sealed class PermissionAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options)
+    : DefaultAuthorizationPolicyProvider(options)
+{
+    private readonly AuthorizationOptions _authorizationOptions = options.Value;
+
+    public override async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+    {
+        AuthorizationPolicy? policy = await base.GetPolicyAsync(policyName);
+
+        if (policy is not null)
+        {
+            return policy;
+        }
+
+        AuthorizationPolicy permissionPolicy = new AuthorizationPolicyBuilder()
+            .AddRequirements(new PermissionRequirement(policyName))
+            .Build();
+
+        _authorizationOptions.AddPolicy(policyName, permissionPolicy);
+
+        return permissionPolicy;
+    }
+}
