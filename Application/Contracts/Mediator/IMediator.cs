76a98cc51aa9719e4d05dd9f717d8f6f2811711b
Nazar Maslianka <maslianka@lqlabs.net>
2025-07-14 16:00:17 +0300

    refact: deneral SendAsync instead SendCommandAsync & SendQueryAsync in IMediator

--- Application/Contracts/Mediator/IMediator.cs
+++ Application/Contracts/Mediator/IMediator.cs
@@ -5,9 +5,5 @@ namespace Application.Contracts.Mediator;
 
 public interface IMediator
 {
-	Task<Result<TResult>> SendCommandAsync<TCommand, TResult>(TCommand command, CancellationToken cancellationToken = default)
-		where TCommand : ICommand<TResult>;
-
-	Task<Result<TResult>> SendQueryAsync<TQuery, TResult>(TQuery query, CancellationToken cancellationToken = default)
-		where TQuery : IQuery<TResult>;
+	Task<Result<TResult>> SendAsync<TResult>(IRequest request, CancellationToken cancellationToken = default);
 }
