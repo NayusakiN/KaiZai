c80d76be241549421a1c73d66d6396962948bdf6
Nazar Maslianka <maslianka@lqlabs.net>
2025-07-24 23:10:08 +0300

    add base components for endpoints in Web.Api

---
+++ Web.Api/Endpoints/IEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Web.Api.Endpoints;
+
+public interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
---
+++ Web.Api/Endpoints/Tags.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.Api.Endpoints;
+
+public static class Tags
+{
+    public const string Users = "Users";
+    public const string Categories = "categories";
+}
