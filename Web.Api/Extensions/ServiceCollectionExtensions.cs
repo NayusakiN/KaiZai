c4b72bb16760cbb4d2e8564c447389e5ca8bb8c1
Nazar Maslianka <maslianka@lqlabs.net>
2025-07-26 11:37:57 +0300

    Web.API: add extensions for results & auth in swagger

---
+++ Web.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace Web.Api.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(o =>
+        {
+            o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "JWT Authentication",
+                Description = "Enter your JWT token in this field",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                BearerFormat = "JWT"
+            };
+
+            o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        }
+                    },
+                    []
+                }
+            };
+
+            o.AddSecurityRequirement(securityRequirement);
+        });
+
+        return services;
+    }
+}
